3b5ceef24671c7569cfdd8a99040d5df918c9556
negative1skunk@gmail.com <negative1skunk@gmail.com>
2021-01-16 20:41:13 -0600

    chapter 11 dun. Built toppings and pizza tables in sanity

---
+++ sanity/schemas/pizza.js
@@ -0,0 +1,41 @@
+import { MdLocalPizza as icon } from 'react-icons/md';
+
+export default {
+  name: 'pizza',
+  title: 'Pizzas',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Pizza name',
+      type: 'string',
+      description: 'Name of pizza',
+    },
+    {
+      name: 'slug',
+      title: 'slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+      description: 'Price in cents',
+      validation: (Rule) => Rule.min(1000).max(50000),
+      //   TODO Custom component
+    },
+  ],
+};
